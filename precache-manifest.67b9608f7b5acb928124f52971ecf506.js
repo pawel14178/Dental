fe949f81c71b354eb5f1ed34182e902595ce24c4
Pawel Jakubiec <pawel143700@gmail.com>
2019-05-27 16:05:04 +0200

    Updates

---
+++ precache-manifest.67b9608f7b5acb928124f52971ecf506.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1257b735b87046228e953d8d831994d2",
+    "url": "/Dental/index.html"
+  },
+  {
+    "revision": "1b363b8349e07b207963",
+    "url": "/Dental/static/css/main.72979903.chunk.css"
+  },
+  {
+    "revision": "d0f1d255fd0f6110c3b3",
+    "url": "/Dental/static/js/2.94e58009.chunk.js"
+  },
+  {
+    "revision": "1b363b8349e07b207963",
+    "url": "/Dental/static/js/main.5a931dfd.chunk.js"
+  },
+  {
+    "revision": "896c0752cb3e04e0755b",
+    "url": "/Dental/static/js/runtime~main.ade5297c.js"
+  },
+  {
+    "revision": "d1fa57df52503a05a893cf9e7300a61c",
+    "url": "/Dental/static/media/background.d1fa57df.png"
+  },
+  {
+    "revision": "02d032619920db1a88080ff6a5f6a0c2",
+    "url": "/Dental/static/media/footer.02d03261.png"
+  },
+  {
+    "revision": "fcf661f3edee0bccd4605577e8c0d76e",
+    "url": "/Dental/static/media/header1.fcf661f3.png"
+  },
+  {
+    "revision": "95cd084ce6f57a382d6c25bfc315112a",
+    "url": "/Dental/static/media/logo.95cd084c.jpg"
+  },
+  {
+    "revision": "41c2c9470e8118cd876a0a09a80dd8be",
+    "url": "/Dental/static/media/portrait.41c2c947.jpg"
+  }
+]);
