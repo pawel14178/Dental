6cb1da3a432b455cb94fa481f5cc28003914ef2b
Pawel Jakubiec <pawel143700@gmail.com>
2019-05-27 15:43:39 +0200

    First uplaod

---
+++ build/precache-manifest.1b46ff5b1550db2a1e6264e9ab7e92bc.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0249fd25af1495129ce0507e2e1f7a69",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7ec807931ab5011f7372",
+    "url": "/static/css/main.771bc9a8.chunk.css"
+  },
+  {
+    "revision": "4cedaf72c576a71c8cf9",
+    "url": "/static/js/2.c1c8de2e.chunk.js"
+  },
+  {
+    "revision": "7ec807931ab5011f7372",
+    "url": "/static/js/main.5f8594c8.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "d1fa57df52503a05a893cf9e7300a61c",
+    "url": "/static/media/background.d1fa57df.png"
+  },
+  {
+    "revision": "02d032619920db1a88080ff6a5f6a0c2",
+    "url": "/static/media/footer.02d03261.png"
+  },
+  {
+    "revision": "fcf661f3edee0bccd4605577e8c0d76e",
+    "url": "/static/media/header1.fcf661f3.png"
+  },
+  {
+    "revision": "95cd084ce6f57a382d6c25bfc315112a",
+    "url": "/static/media/logo.95cd084c.jpg"
+  },
+  {
+    "revision": "41c2c9470e8118cd876a0a09a80dd8be",
+    "url": "/static/media/portrait.41c2c947.jpg"
+  }
+]);
